2c536598cc5f7de13e9e0c5a967de219b93fb517
Pascal <pilzinsel64@gmx.de>
2024-12-16 18:28:21 +0100

    feat: log asset path on error

--- src/main/java/glowredman/txloader/RemoteHandler.java
+++ src/main/java/glowredman/txloader/RemoteHandler.java
@@ -85,7 +85,7 @@ class RemoteHandler {
                 try {
                     jAsset.download(file);
                 } catch (Exception e) {
-                    TXLoaderCore.LOGGER.error("Failed to get asset!", e);
+                    TXLoaderCore.LOGGER.error("Failed to get asset! Path: " + asset.resourceLocation, e);
                     failed++;
                 }
                 continue;
@@ -121,7 +121,7 @@ class RemoteHandler {
                 InputStream is = jarFile.getInputStream(jarFile.getJarEntry("assets/" + asset.resourceLocation));
                 FileUtils.copyInputStreamToFile(is, file);
             } catch (Exception e) {
-                TXLoaderCore.LOGGER.error("Failed to extract asset from jar!", e);
+                TXLoaderCore.LOGGER.error("Failed to extract asset from jar! Path: " + asset.resourceLocation, e);
                 failed++;
                 continue;
             }
