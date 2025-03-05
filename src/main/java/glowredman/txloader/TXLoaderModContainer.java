7b987bec7dfc12ce86b4e7b0d0bc3f210fa22467
glowredman <35727266+glowredman@users.noreply.github.com>
2025-03-05 08:31:11 +0100

    Fix Metadata not working outside of the Development Environment (#16)

--- src/main/java/glowredman/txloader/TXLoaderModContainer.java
+++ src/main/java/glowredman/txloader/TXLoaderModContainer.java
@@ -1,6 +1,7 @@
 package glowredman.txloader;
 
 import java.io.File;
+import java.util.jar.JarFile;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
@@ -9,6 +10,7 @@ import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.MetadataCollection;
+import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.versioning.VersionParser;
@@ -18,9 +20,22 @@ import glowredman.txloader.progress.ProgressBarProxy;
 public class TXLoaderModContainer extends DummyModContainer {
 
     public TXLoaderModContainer() {
-        super(
-                MetadataCollection.from(TXLoaderModContainer.class.getResourceAsStream("/mcmod.info"), "TX Loader")
-                        .getMetadataForId("txloader", null));
+        super(getModMetadata());
+    }
+
+    private static ModMetadata getModMetadata() {
+        try (JarFile jar = new JarFile(TXLoaderCore.modFile)) {
+            return MetadataCollection.from(jar.getInputStream(jar.getEntry("mcmod.info")), "TX Loader")
+                    .getMetadataForId("txloader", null);
+        } catch (Exception e) {
+            TXLoaderCore.LOGGER.warn("Failed to get mod metadata", e);
+        }
+        ModMetadata fallback = new ModMetadata();
+        fallback.description = "This is a fallback description! Something went wrong while getting this mod's metadata. Refer to the log and report this issue to the mod's author!";
+        fallback.modId = "txloader";
+        fallback.name = "TX Loader";
+        fallback.version = "0.0-FALLBACK";
+        return fallback;
     }
 
     @Override
