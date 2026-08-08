d73e13e60f8d1880b85e76e91ff46813bb872851
glowredman <35727266+glowredman@users.noreply.github.com>
2026-08-08 13:47:55 +0200

    Download to temp file, then move if successful

--- src/main/java/glowredman/txloader/RemoteHandler.java
+++ src/main/java/glowredman/txloader/RemoteHandler.java
@@ -255,11 +255,15 @@ class RemoteHandler {
 
     private static void download(String url, Path path) throws IOException {
         TXLoaderCore.LOGGER.info("Downloading {} to {}", url, path);
+        Path temp = Files.createTempFile(path.getParent(), path.getFileName().toString(), null);
         URLConnection connection = new URL(url).openConnection();
         connection.setConnectTimeout(CONNECT_TIMEOUT);
         connection.setReadTimeout(READ_TIMEOUT);
         try (InputStream is = connection.getInputStream()) {
-            Files.copy(is, path, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(is, temp, StandardCopyOption.REPLACE_EXISTING);
+        }
+        Files.move(path, temp, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+        Files.deleteIfExists(temp);
     }
 
     private static <T> void resetOnFailure(Map<String, CompletableFuture<T>> map, String key, T result) {
